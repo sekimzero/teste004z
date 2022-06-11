d8b3365b431b9c318ee46760e5e9ca5809dd107f
salomao kim <sekim.maua@gmail.com>
2022-06-11 00:59:25 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string retornarDiagnosticoDoUsuario(double alturaDoUsuario, double
+pesoDoUsuario)
+{
+}
+// Utilize a mensagem a seguir como base do retorno esperado:
+// Seu diagnóstico é XXXXXX.
+Console.WriteLine($"Seu diagnóstico é {retornarDiagnosticoDoUsuario()}");
